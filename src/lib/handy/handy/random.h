e15b159e09510dc35188caf61f5b1398e851cf8f
Adrien <Adnn@users.noreply.github.com>
2020-10-21 11:55:29 +0200

    Random seed can be set explicitly

--- src/lib/handy/handy/random.h
+++ src/lib/handy/handy/random.h
@@ -8,10 +8,12 @@ template <class T_distribution = std::uniform_int_distribution<int>>
 struct Randomizer
 {
     typedef typename T_distribution::result_type scalar_type;
+    typedef std::default_random_engine engine_type;
 
-    Randomizer(scalar_type aMin, scalar_type aMax) :
+    Randomizer(scalar_type aMin, scalar_type aMax, scalar_type aSeed = engine_type::default_seed) :
         mMin(aMin),
         mMax(aMax),
+        mEngine(aSeed),
         mDistribution(aMin, aMax)
     {}
 
