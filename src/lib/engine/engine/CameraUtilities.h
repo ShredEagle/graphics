4e9021731746d5a882183e8180b4b82a9bf9c70d
Adrien <Adnn@users.noreply.github.com>
2021-09-23 16:45:55 +0200

    Use new projection facility in Camera utilities.

--- src/lib/engine/engine/CameraUtilities.h
+++ src/lib/engine/engine/CameraUtilities.h
@@ -14,10 +14,8 @@ template <class T_engine2D>
 void setViewedRectangle(T_engine2D & aEngine, math::Rectangle<GLfloat> aViewedRectangle)
 {
     aEngine.setCameraTransformation(math::trans2d::translate(- aViewedRectangle.bottomLeft().as<math::Vec>()));
-    aEngine.setProjectionTransformation(math::trans2d::window<GLfloat>(
-        { {  0.f,  0.f }, aViewedRectangle.dimension() },
-        { { -1.f, -1.f }, { 2.f, 2.f } }
-    ));
+    aEngine.setProjectionTransformation(
+        math::trans2d::orthographicProjection<GLfloat>({ {  0.f,  0.f }, aViewedRectangle.dimension() }));
 }
 
 
