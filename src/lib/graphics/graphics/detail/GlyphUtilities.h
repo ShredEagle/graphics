5bb13fab1db9c78f5c1ad3b1255888d6d32c397c
Adrien <Adnn@users.noreply.github.com>
2021-11-17 10:29:19 +0100

    Clamp TextureRibon to edges when used to atlas glyphs.

--- src/lib/graphics/graphics/detail/GlyphUtilities.h
+++ src/lib/graphics/graphics/detail/GlyphUtilities.h
@@ -38,8 +38,10 @@ inline TextureRibon make_TextureRibon(math::Size<2, GLint> aDimensions, GLenum a
     allocateStorage(ribon.texture, aInternalFormat, aDimensions.width(), aDimensions.height());
 
     bind(ribon.texture);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_RECTANGLE, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_RECTANGLE, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_RECTANGLE, GL_TEXTURE_WRAP_S, GL_CLAMP);
+    glTexParameteri(GL_TEXTURE_RECTANGLE, GL_TEXTURE_WRAP_T, GL_CLAMP);
 
     return ribon;
 }
