03f3ac9b9e146816e1a65d496e04af5cc60dc87c
Adrien <Adnn@users.noreply.github.com>
2021-11-26 18:09:57 +0100

    Generalize ParameterAnimation template class, add number_test animation.

--- src/app/samples/sprite-animation/sprite-animation/Scene.h
+++ src/app/samples/sprite-animation/sprite-animation/Scene.h
@@ -15,71 +15,127 @@ namespace ad {
 namespace graphics {
 
 
-// TODO rename
+const std::string gAnimationName = "number_test";
+
+
 namespace periodicity
 {
     template <class T_parameter>
-    class Periodic_base
+    struct Repeat
     {
-    public:
-        Periodic_base(T_parameter aDuration) :
-            mDuration{std::move(aDuration)}
-        {}
-
-    protected:
-        T_parameter mDuration;
-    };
-
-    template <class T_parameter>
-    class Repeat : Periodic_base<T_parameter>
-    {
-        using Periodic_base<T_parameter>::Periodic_base;
-
-    public:
-        // TODO find a name
-        T_parameter operator()(T_parameter aAbsoluteValue) const
+        // TODO give a name?
+        T_parameter operator()(T_parameter aPeriod, T_parameter aAbsoluteValue) const
         {
-            return aAbsoluteValue - std::floor(aAbsoluteValue/mDuration) * mDuration;
+            return aAbsoluteValue - std::floor(aAbsoluteValue/aPeriod) * aPeriod;
         }
     };
 
     template <class T_parameter>
-    class PingPong : Periodic_base<T_parameter>
+    struct PingPong
     {
-        using Periodic_base<T_parameter>::Periodic_base;
-
-    public:
-        // TODO find a name
-        T_parameter operator()(T_parameter aAbsoluteValue) const
+        T_parameter operator()(T_parameter aPeriod, T_parameter aAbsoluteValue) const
         {
-            T_parameter doubleDuration = 2 * mDuration;
+            T_parameter doubleDuration = 2 * aPeriod;
             T_parameter doublePeriod =
                 aAbsoluteValue - std::floor(aAbsoluteValue/doubleDuration) * doubleDuration;
 
-            return doublePeriod - 2 * std::max(T_parameter{0}, doublePeriod - mDuration);
+            return doublePeriod - 2 * std::max(T_parameter{0}, doublePeriod - aPeriod);
         }
     };
 } // namespace periodicity
 
-template <class T_parameter,
-          class T_periodicity,
-          class T_easeFunctor = math::ease::Linear<T_parameter>>
 
-class UnboundParameterAnimation
+enum AnimationResult
+{
+    FullRange = 0,
+    Clamped,
+};
+
+
+template <class T_parameter, AnimationResult N_resultRange>
+class ParameterAnimation_ResultHelper
 {
 public:
     using Result_type = T_parameter;
 
-    
-    explicit UnboundParameterAnimation(T_parameter aSpeedFactor, T_periodicity aBehaviour) :
-        mBehaviour{std::move(aBehaviour)},
-        mSpeedFactor{aSpeedFactor}
+protected:
+    ParameterAnimation_ResultHelper(T_parameter aSpeed) :
+        mSpeed{std::move(aSpeed)}
+    {}
+
+    T_parameter mSpeed;
+};
+
+
+// Specialization when the result is to be clamped.
+template <class T_parameter>
+class ParameterAnimation_ResultHelper<T_parameter, Clamped>
+{
+    // TODO remove math::
+    using Result_type = math::Clamped<T_parameter>;
+
+protected:
+    static constexpr T_parameter mSpeed{1};
+};
+
+
+/// \brief An empty class template.
+template <class>
+struct None
+{};
+
+
+// TODO potential optimization (only speed or period member) for no-easing/no-periodicity
+template <class T_parameter,
+          AnimationResult  N_resultRange = FullRange,
+          template <class> class TT_periodicity = None,
+          template <class> class TT_easeFunctor = None>
+class ParameterAnimation : ParameterAnimation_ResultHelper<T_parameter, N_resultRange>
+{
+    using Base_t = ParameterAnimation_ResultHelper<T_parameter, N_resultRange>;
+
+public:
+    template <bool N_isClamped = IsClamped()>
+    explicit ParameterAnimation(T_parameter aPeriod, std::enable_if_t<N_isClamped>* = nullptr) :
+        mPeriod{aPeriod}
+    {}
+
+    template <bool N_isClamped = IsClamped()>
+    explicit ParameterAnimation(T_parameter aPeriod, T_parameter aSpeed = T_parameter{1},
+                                std::enable_if_t<!N_isClamped>* = nullptr) :
+        Base_t{aSpeed},
+        mPeriod{aPeriod}
     {}
 
 
+    // Note: This is trying its best to implement the overall animation logic below:
+    //   amplitude * easeFunctor(normalize(periodicBehaviour(speedFactor * aInput)));
+    // while statically disabling what is not relevant in the specific template instantiation.
     Result_type at(T_parameter aInput) const
     {
-        return T_easeFunctor::ease(mBehaviour(mSpeedFactor * aInput));
+        aInput *= mSpeed;
+
+        if constexpr(IsPeriodic())
+        {
+            aInput = mPeriodicBehaviour(mPeriod, aInput);
+        }
+
+        if constexpr(IsEasing())
+        {
+            static_assert(!std::is_integral_v<T_parameter>,
+                          "Integral parameters require more work regarding normalization.");
+
+            // Need to normalize the easing input
+            aInput = TT_easeFunctor<T_parameter>::ease(aInput / mPeriod);
+
+            if constexpr(N_resultRange == FullRange)
+            {
+                // Then expand it back to the full amplitude if result is not full range
+                aInput *= mPeriod;
+            }
+        }
+
+        return aInput;
     }
 
 
@@ -97,9 +153,25 @@ public:
 
 
 private:
-    T_periodicity mBehaviour;
-    T_parameter mSpeedFactor;
+    static constexpr bool IsEasing()
+    {
+        return !std::is_same_v<TT_easeFunctor<void>, None<void>>;
+    }
+
+    static constexpr bool IsPeriodic()
+    {
+        return !std::is_same_v<TT_periodicity<void>, None<void>>;
+    }
+
+    static constexpr bool IsClamped()
+    {
+        return N_resultRange == Clamped;
+    }
+
+    T_parameter mPeriod;
     T_parameter mAccumulatedInput{0};
+    TT_periodicity<T_parameter> mPeriodicBehaviour; // empty class for basic initial cases (Repeat, PingPong)
+                                                    // but leaves room for more potential other scenarios.
 };
 
 
@@ -179,16 +251,15 @@ public:
         mSpriting{std::move(aRenderResolution)}
     {
         arte::AnimationSpriteSheet sheet = 
-            arte::AnimationSpriteSheet::LoadAseFile(resource::pathFor("animations/run.json"));
+            arte::AnimationSpriteSheet::LoadAseFile(resource::pathFor("animations/" + gAnimationName + ".json"));
 
         mAnimator.load(sheet, mSpriting);
 
         // Animation is given in milliseconds, so multiply speed by 10^3
         mAnimationParameter =
-            //UnboundParameterAnimation<double, periodicity::PingPong<double>>{
-            UnboundParameterAnimation<double, periodicity::Repeat<double>>{
-                1000,
-                {mAnimator.get("run").totalDuration}
+            ParameterAnimation_t{
+                mAnimator.get(gAnimationName).totalDuration,
+                200
         };
     }
 
@@ -196,7 +267,7 @@ public:
     {
         Spriting::Instance instance = Spriting::Instance{
             {0, 0},
-            mAnimator.at("run", mAnimationParameter->advance(aDelta)),
+            mAnimator.at(gAnimationName, mAnimationParameter->advance(aDelta)),
         };
         mSpriting.updateInstances(std::vector<Spriting::Instance>{instance});
     }
@@ -207,11 +278,18 @@ public:
     }
 
 private:
+    // TODO hardcoded type here is not ideal
+    using ParameterAnimation_t =
+        //ParameterAnimation<double, FullRange>;
+        ParameterAnimation<double, FullRange, periodicity::Repeat>;
+        //ParameterAnimation<double, FullRange, periodicity::PingPong>;
+        //ParameterAnimation<double, FullRange, periodicity::Repeat, math::ease::SmoothStep>;
+        //ParameterAnimation<double, FullRange, periodicity::PingPong, math::ease::SmoothStep>;
+        //ParameterAnimation<double, FullRange, None, math::ease::SmoothStep>;
+
     Spriting mSpriting;
     Animator mAnimator;
-    // TODO hardcoded type here is not ideal
-    std::optional<UnboundParameterAnimation<double, periodicity::Repeat<double>>> mAnimationParameter;
-    //std::optional<UnboundParameterAnimation<double, periodicity::PingPong<double>>> mAnimationParameter;
+    std::optional<ParameterAnimation_t> mAnimationParameter;
 };
 
 
