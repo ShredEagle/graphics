08cb20ed53094105a415f609ca7042aebd23adc5
Adrien <Adnn@users.noreply.github.com>
2022-02-21 17:43:07 +0100

    Implement clear(Texture).

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -21,7 +21,7 @@ class GraphicsConan(ConanFile):
         "build_tests": False,
         "glad:gl_version": "4.1",
         # Note: macos only provides GL_ARB_texture_storage and GL_ARB_internalformat_query
-        "glad:extensions": "GL_KHR_debug, GL_ARB_texture_storage",
+        "glad:extensions": "GL_KHR_debug, GL_ARB_texture_storage, GL_ARB_clear_texture",
     }
 
     requires = (
