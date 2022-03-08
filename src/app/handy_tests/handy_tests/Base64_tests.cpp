ceb6e0f191e5b8527596bc2f9dee28461c56de38
Adrien <Adnn@users.noreply.github.com>
2022-03-08 11:11:19 +0100

    [handy] Provide base64 decoder, via embedded boost/beast header.

---
+++ src/app/handy_tests/handy_tests/Base64_tests.cpp
@@ -0,0 +1,25 @@
+#include "catch.hpp"
+
+#include <handy/Base64.h>
+
+
+using namespace ad;
+using namespace ad::handy;
+
+
+SCENARIO("Base64 decoding")
+{
+    GIVEN("A Base64 encoded string.")
+    {
+        const std::string sourceData{"Base64 test payload."};
+        const std::string encoded{"QmFzZTY0IHRlc3QgcGF5bG9hZC4="}; 
+
+        THEN("It can be decoded back to the original binary.")
+        {
+            std::vector<std::byte> decoded = base64::decode(encoded);
+            std::string decodedToString{reinterpret_cast<char *>(decoded.data()), decoded.size()};
+            REQUIRE(decodedToString == sourceData);
+            REQUIRE_FALSE(decodedToString == "Non source data");
+        }
+    }
+}
